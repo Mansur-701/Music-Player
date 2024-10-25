15d412261d827d4c3b28b95fde292da332d88adb
Mansur-701 <m.mansur1@icloud.com>
2024-10-25 10:02:03 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Volume = new System.Windows.Forms.Label();
+            this.VolumeBar = new System.Windows.Forms.TrackBar();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.musicPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timerPlayback = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musicPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Volume);
+            this.panel1.Controls.Add(this.VolumeBar);
+            this.panel1.Controls.Add(this.btnPause);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Location = new System.Drawing.Point(12, 588);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1551, 153);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(1268, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Length 0:00 / 0:00";
+            // 
+            // Volume
+            // 
+            this.Volume.AutoSize = true;
+            this.Volume.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Volume.Font = new System.Drawing.Font("Tw Cen MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Volume.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Volume.Location = new System.Drawing.Point(958, 9);
+            this.Volume.Name = "Volume";
+            this.Volume.Size = new System.Drawing.Size(84, 25);
+            this.Volume.TabIndex = 5;
+            this.Volume.Text = "Volume";
+            // 
+            // VolumeBar
+            // 
+            this.VolumeBar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.VolumeBar.Location = new System.Drawing.Point(1009, 54);
+            this.VolumeBar.Name = "VolumeBar";
+            this.VolumeBar.Size = new System.Drawing.Size(276, 69);
+            this.VolumeBar.TabIndex = 4;
+            this.VolumeBar.Scroll += new System.EventHandler(this.VolumeBar_Scroll);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPause.Font = new System.Drawing.Font("Tw Cen MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnPause.Location = new System.Drawing.Point(687, 53);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(152, 82);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStop.Font = new System.Drawing.Font("Tw Cen MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnStop.Location = new System.Drawing.Point(466, 53);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(152, 82);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStart.Font = new System.Drawing.Font("Tw Cen MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStart.Location = new System.Drawing.Point(249, 53);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(152, 82);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLoad.Font = new System.Drawing.Font("Tw Cen MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLoad.Location = new System.Drawing.Point(32, 53);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(152, 82);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Tw Cen MT", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 37;
+            this.listBox1.Location = new System.Drawing.Point(12, 130);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1476, 115);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tw Cen MT", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(656, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 33);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Imported Songs";
+            // 
+            // musicPlayer
+            // 
+            this.musicPlayer.Enabled = true;
+            this.musicPlayer.Location = new System.Drawing.Point(12, -1);
+            this.musicPlayer.Name = "musicPlayer";
+            this.musicPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("musicPlayer.OcxState")));
+            this.musicPlayer.Size = new System.Drawing.Size(1476, 128);
+            this.musicPlayer.TabIndex = 0;
+            this.musicPlayer.Visible = false;
+            this.musicPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.musicPlayer_PlayStateChange);
+            // 
+            // timerPlayback
+            // 
+            this.timerPlayback.Interval = 850;
+            this.timerPlayback.Tick += new System.EventHandler(this.timerPlayback_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.resized_music_symbol_image;
+            this.ClientSize = new System.Drawing.Size(1562, 753);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.musicPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musicPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+      
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer musicPlayer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TrackBar VolumeBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Volume;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timerPlayback;
+    }
+}
+
